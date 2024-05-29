e28e4e2b0653cb51220e5a21c6c9fa79f2c92909
Krish <krishnayadav48065@gmail.com>
2024-05-30 02:17:40 +0530

    project complete

--- my-node-project/index.js
+++ my-node-project/index.js
@@ -2,20 +2,26 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 // Create a transporter object using the default SMTP transport
-let transporter = nodemailer.createTransport({
-  service: 'gmail', // Use 'gmail' if you're using a Gmail account, or configure your own SMTP service
-  auth: {
-    user: process.env.EMAIL, // Your email
-    pass: process.env.PASSWORD, // Your email password or app-specific password
-  },
-});
+const transporter = nodemailer.createTransport({
+    service:'gmail',
+    host: "smtp.gmail.email",
+    port: 587,
+    secure: false, // Use `true` for port 465, `false` for all other ports
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
 
 // Define the email options
 let mailOptions = {
-  from: '"Sender Name" <your-email@gmail.com>', // Sender address
-  to: 'recipient-email@example.com', // List of receivers
-  subject: 'Hello from Node.js', // Subject line
-  text: 'Hello world?', // Plain text body
+  from: {
+    name : "Krishna",
+    address:process.env.EMAIL,
+  }, // Sender address
+  to: 'krishnayadav2nd@gmail.com', // List of receivers
+  subject: 'demo subject', // Subject line
+  text: 'Hello world, ITS THIRD MAIL', // Plain text body
   html: '<b>Hello world?</b>', // HTML body
 };
 
